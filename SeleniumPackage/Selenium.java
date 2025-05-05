b26f23fbb48a3f3758888a9f6aa61d7e55c4fd9c
VeronLoboA <xavioveron@gmail.com>
2025-05-05 13:23:51 +0530

    Add files via upload
    week2day1

---
+++ SeleniumPackage/Selenium.java
@@ -0,0 +1,48 @@
+package SeleniumPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class Selenium {
+	public static void main(String[] args) {
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("guest");
+		ChromeDriver driver=new ChromeDriver(options);
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		driver.findElement(By.partialLinkText("CRM")).click();
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("saranya");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("S");
+		//by index
+		WebElement sourceDD = driver.findElement(By.id("createLeadForm_dataSourceId"));
+		Select select=new Select(sourceDD);
+		select.selectByIndex(4);
+		//by value
+		WebElement industryDD = driver.findElement(By.id("createLeadForm_industryEnumId"));
+		Select select1=new Select(industryDD);
+		select1.selectByValue("IND_MEDIA");
+		
+		WebElement ownerDD = driver.findElement(By.id("createLeadForm_ownershipEnumId"));
+		Select select2=new Select(ownerDD);
+		select2.selectByVisibleText("Corporation");
+		}
+
+}
